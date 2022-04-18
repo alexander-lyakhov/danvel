cda90e103699be93f261fcdc1249fbd393390267
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2022-04-18 14:30:29 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <aside id="sidebar">
+    <div class="logo"></div>
+    <nav>
+    </nav>
+  </aside>
+  <main>
+    <page-header></page-header>
+    <router-view />
+  </main>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+import pageHeader from '@/components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    pageHeader
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+#sidebar {
+  background: #000;
+  width: 320px;
+
+  .logo {
+    background: #C2F14D;
+    height: 96px;
+  }
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+main {
+  background: #808080;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+﻿<template>
+  <header></header>
+</template>
+
+<script>
+
+export default {
+  name: 'PageHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+header {
+  background: #008080;
+  width: 100%;
+  height: 96px;
+}
+</style>
